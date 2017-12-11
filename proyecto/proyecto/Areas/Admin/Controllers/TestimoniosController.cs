ae5ab0954e8873987b4c6f980ad3fbde0859641c
Paul Orejuela <orejuelaip@gmail.com>
2017-12-10 21:04:49 -0500

    repo de portafolio

---
+++ proyecto/proyecto/Areas/Admin/Controllers/TestimoniosController.cs
@@ -0,0 +1,60 @@
+﻿using Helper;
+using Model;
+using proyecto.Areas.Admin.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proyecto.Areas.Admin.Controllers
+{
+    [Autenticado]
+    public class TestimoniosController : Controller
+    {
+        private Testimonio testimonio = new Testimonio();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Listar(AnexGRID grid)
+        {
+            return Json(testimonio.Listar(grid, SessionHelper.GetUser()));
+        }
+
+        public ActionResult Crud(int id = 0)
+        {
+            Testimonio testmonio = new Testimonio();
+            testimonio.Usuario_id = 3;
+            if (id != 0 )
+            {
+                testimonio = testimonio.Obtener(id);
+            }
+            return View(testimonio);
+        }
+
+        public JsonResult Guardar(Testimonio model)
+        {
+            var rm = new ResponseModel();
+
+            if (ModelState.IsValid)
+            {
+                rm = model.Guardar();
+
+                if (rm.response)
+                {
+                    rm.href = Url.Content("~/admin/testimonios/");
+                }
+            }
+
+            return Json(rm);
+        }
+
+        public JsonResult Eliminar(int id)
+        {
+            return Json(testimonio.Eliminar(id));
+        }
+    }
+}
---
+++ proyecto/proyecto/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿ 
+using System.Web.Mvc;
+ 
+using proyecto.Areas.Admin.Filters;
+
+namespace proyecto.Controllers
+{
+    public class DefaultController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+          
+            return View();
+        }
+    }
+}
